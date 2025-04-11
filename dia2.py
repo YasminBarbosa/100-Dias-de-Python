c14d10ed480467370ff8f1d24ad5a07593b28079
YasminBarbosa <yasminbarbosa008@gmail.com>
2025-04-11 16:12:04 -0300

    Dia 2 de 100 com Python

---
+++ dia2.py
@@ -0,0 +1,8 @@
+#Escreva um programa que calcule a média aritmética de dois números
+
+num1 = int(input('Insira um 1° número: '))
+num2 = int(input('Insira um 2° número: '))
+
+media = (num1 + num2 ) / 2
+
+print('Esta é a sua Média Aritmética: ', int(media))
