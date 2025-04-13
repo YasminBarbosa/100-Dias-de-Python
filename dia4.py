cd457783219495c235a26bb4d692eb9d0f657935
YasminBarbosa <yasminbarbosa008@gmail.com>
2025-04-13 17:30:24 -0300

    Dia 4 de 100 com Python

---
+++ dia4.py
@@ -0,0 +1,3 @@
+#Escreva um programa que calcule a média geométrica de três números inseridos pelo usuário
+
+#INGLÊS: Write a program that calculates the geometric mean of three numbers entered by the user
